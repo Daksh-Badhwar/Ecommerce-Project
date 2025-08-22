f468ad98d3932213a7b2880682bfada69df74422
Daksh-Badhwar <badhwardakshhwhlpj369@gmail.com>
2025-08-22 17:41:38 +0530

    Testimonial Image setup

--- vite-project/src/components/testimonial/Testimonials.jsx
+++ vite-project/src/components/testimonial/Testimonials.jsx
@@ -26,7 +26,7 @@ const Testimonial = () => {
                         {/* Testimonial 2 */}
                         <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
                             <div className="h-full text-center">
-                                <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://www.devknus.com/img/gawri.png" />
+                                <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://course.devknus.com/img/gawri.png" />
                                 <p className="leading-relaxed">"This is my go-to store for everything from fashion to home essentials. The deals are great and the support team is very responsive. Highly recommended!"</p>
                                 <span className="inline-block h-1 w-10 rounded bg-pink-500 mt-6 mb-4" />
                                 <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm uppercase">Priya Sharma</h2>
@@ -37,10 +37,10 @@ const Testimonial = () => {
                         {/* Testimonial 3 */}
                         <div className="lg:w-1/3 lg:mb-0 p-4">
                             <div className="h-full text-center">
-                                <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://firebasestorage.googleapis.com/v0/b/devknus-official-database.appspot.com/o/images%2FScreenshot%202023-07-07%20at%202.20.32%20PM-modified.png?alt=media&token=324ddd80-2b40-422c-9f1c-1c1fa34943fa" />
+                                <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://course.devknus.com/img/gawri.png" />
                                 <p className="leading-relaxed">"Absolutely love shopping here! The products always arrive on time and look exactly like the pictures. Great quality and amazing customer service. I’ve become a regular customer!"</p>
                                 <span className="inline-block h-1 w-10 rounded bg-pink-500 mt-6 mb-4" />
-                                <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm uppercase">Aarav Mehta </h2>
+                                <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm uppercase">Parvati Mehta </h2>
                                 <p className="text-gray-500">Mumbai</p>
                             </div>
                         </div>
